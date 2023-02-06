a8e092b4089b2604cc17215f7a86696f987d9c8a
Gustavo Dalpasquale <gustavo2.noriler@hotmail.com>
2023-02-06 13:01:49 -0300

    Add teams slide section

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 import MenuMobile from "./modules/menu-mobile.js";
 import ScrollAnimation from "./modules/scroll-animation.js";
 import ScrollSmooth from "./modules/scroll-smooth.js";
+import Slide from "./modules/slide.js";
 
 const menuMobile = new MenuMobile(
   ".menu-button",
@@ -14,3 +15,7 @@ scrollAnimation.init();
 
 const scrollSmooth = new ScrollSmooth(".navbar a[href^='#']");
 scrollSmooth.init();
+
+const slide = new Slide(".slide", ".slide-wrapper");
+slide.init();
+slide.addControl(".custom-controls");
